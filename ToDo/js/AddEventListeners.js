1478b68659b14cc403a7399c9919e07f780d6711
szabloreki <oscar1tolowski@gmail.com>
2017-05-08 19:02:22 +0200

    Add files via upload
    So this update adding new Feature like adding work people  and ordering the code fragmenting into small pieces

---
+++ ToDo/js/AddEventListeners.js
@@ -0,0 +1,43 @@
+class Listeners {
+    constructor () {
+        this.UserAdd = document.querySelector('#AddUser');
+        this.inputText = document.querySelector('#AddUserText');
+        this.showWorkers = document.querySelector('#CheckWorkers');
+        this.userNameWork = document.querySelector('#UserNameWork');
+        this.workText = document.querySelector('#WorkText');
+        this.addWork = document.querySelector('#AddWork');
+    }
+    addListener (element,functioN){
+        element.addEventListener('click',functioN,false);
+    }
+    showElement(element){
+        console.log(element);
+    }
+}
+let listener = new Listeners();
+listener.showElement(listener.inputText);
+listener.addListener(listener.UserAdd, function (){
+        let name = listener.inputText.value;
+        if(name.length === 0)
+            {
+                console.log('Nie podałeś Nazwy!');
+                console.log(people)
+                return;
+            }
+        optionsPeople.addHuman(name);
+        console.log(people);
+})
+listener.addListener(listener.showWorkers, function(){
+        optionsPeople.showEveryName();
+        let arrayDOM = genereteDOM.nameWorkers();    
+        gui.showAllName(arrayDOM);
+})
+listener.addListener(listener.addWork, function (){
+    let name = listener.userNameWork.value;
+    let workString = listener.workText.value;
+    let arrayName = [];
+    arrayName.push(name);
+    console.log(name, workString);
+    console.log(this);
+    optionsPeople.addWork(arrayName,workString);
+})
