590ee050ef585325c0c670f33a552f7c3eaa1e15
szabloreki <oscar1tolowski@gmail.com>
2017-05-19 20:25:50 +0200

    Delete Console.js

--- ToDo/js/Console.js
+++
@@ -1,9 +0,0 @@
-class Console {
-    constructor (){
-        this.changeText = document.querySelector('#Information')
-        this.changeInformation = function (string){
-        this.changeText.innerHTML = string;
-        }
-    }
-}
-let information = new Console();
