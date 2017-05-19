169e04b040d1bc1026d1132daa4958046b924c0d
szabloreki <oscar1tolowski@gmail.com>
2017-05-19 20:35:58 +0200

    So this the big  update [NOT MISTAKE]

--- server Aplication/app.js
+++ server Aplication/app.js
@@ -6,51 +6,20 @@ let mongojs = require('mongojs');
 let db = mongojs('workers', ['users']);
 let app = express();
 
-/*
-let logger = function(req, res, next){
-	console.log('logging..');
-	next();
-
-}
-*/
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
-
-//Set Static path
-app.use(express.static(path.join(__dirname, 'public')))
-
-
-//Global
 
 app.use(function (req, res, next){
 	res.locals.errors = null;
 	next();
 })
 
+app.use("/public/", function (req, res) {
+    res.sendFile(path.join(__dirname + req.originalUrl))
+    
+    console.log(path.join(__dirname + req.originalUrl))
+})
 
-
-
-/*let people = [
-	{
-		name: 'Jefff',
-		age: 30,
-	},
-	{
-		name: 'Harry',
-		age: 25,
-	},
-	{
-		name: 'John',
-		age: 40
-	}
-]
-*/
-
-//Express Validator
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
       var namespace = param.split('.')
@@ -68,52 +37,51 @@ app.use(expressValidator({
   }
 }));
 
-
-/*let users = [
-	{	id: 1,
-		first_name: 'John',
-		last_name: 'Gregory',
-		email: 'john@fuckyou'
-	},
-	{	id: 2,
-		first_name: 'Lucy',
-		last_name: 'Oly',
-		email: 'john@fuckandyou'
-	},
-	{	id: 3,
-		first_name: 'Lidia',
-		last_name: 'Soriet',
-		email: 'john@fucktoyou'
-	}
-]*/
+app.get('/style.css', function (req, res){
+	res.sendFile(path.join(__dirname + '/style.css'))
+})
 
 app.get('/',  function(req, res){
-
 	db.users.find(function (err, docs) {
 		console.log(docs);
-		res.render('index', {
-			title: 'Customers',
-			users: docs
-		})
+		//res.render('index', {
+		//	title: 'Customers',
+		//	users: docs
+	///	})
 	})
+	res.sendFile(path.join(__dirname + '/index.html'));
 	//res.json(people)
 }); 
 
+///Changing DB
+app.get('/server/ajax' ,function (req, res){
+		db.users.find(function (err, docs) {
+		console.log(docs);
+		res.json(docs);
+	})
+})
+
+app.post('/addToDataBase' ,function (req, res){
+		console.log(req);
+		console.log(req.body);
+		let  workers = req.body;
+		db.users.remove({});
+		db.users.insert(workers, function (err, result){
+			if(err)
+			{
+				console.log(err);
+			}
+			console.log(result);
+		});
+})
 
 app.post('/users/add', function (req, res){
-
 	req.checkBody('first_name', 'First name is Required!').notEmpty();
 	req.checkBody('last_name', 'last name is Required!').notEmpty();
 	req.checkBody('email', 'email is Required!').notEmpty();
-	
 	let errors = req.validationErrors();
-
 	if(errors) {
-
 		res.redirect('/')
-
-
-
 		res.render('index', {
 			title: 'Customers',
 			users: users,
@@ -126,8 +94,7 @@ app.post('/users/add', function (req, res){
 		last_name: req.body.last_name,
 		email: req.body.email
 		}
-		console.log('SUCCESS');
-	
+		console.log('SUCCESS');	
 	db.users.insert(newUser, function (err, result) {
 			if(err){
 				console.log(err);
@@ -135,10 +102,7 @@ app.post('/users/add', function (req, res){
 
 			res.redirect('/')
 	});
-
 	}
-
-
 	//console.log(req.body.first_name);
 });
 
